3f92346dc74411edc9c9303504406fc13ebf21ef
shubhamkakkar <shubham.kakkar98@gmail.com>
2019-10-05 11:13:40 +0530

    uuif

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import express from 'express';
-import * as bodyParser from 'body-parser';
+import 'dotenv/config';
 import cors from 'cors';
+import uuidv4 from 'uuid';
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+
+import schema from './schema';
 import resolvers from './resolvers';
-import typeDefs from './schema/temp';
+import models from './models';
 
-const server = new ApolloServer({ resolvers, typeDefs });
 const app = express();
-app.use(cors(), bodyParser.json());
 
-server.applyMiddleware({ app });
+app.use(cors());
 
-app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-);
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    context: {
+        models,
+        me: models.users[1],
+    },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+});
